9ed9f081540aa2da33c790cc91925fd6f1aa64f8
igodlab <igodlab@gmail.com>
2026-03-30 10:23:24 -0700

    Full solution to tough problem 2.63
    Smart masks to write custom logical and arithmetic shifts

--- src/ch02/c-solutions/prob2-63.c
+++ src/ch02/c-solutions/prob2-63.c
@@ -6,7 +6,7 @@ not including right shifts or division. Function sra performs an arithmetic righ
 using a logical right shift (given by value `xsrl`), followed by other operations not 
 including right shifts or division. You may use the computation 8*sizeof (int) to 
 determine w, the number of bits in data type int. The shift amount k can range 
-from O to w - l. 
+from O to w - 1.
 
 ```c
 unsigned srl (unsigned x , int k) { 
@@ -37,8 +37,8 @@ unsigned srl(unsigned x, int k) {
   
   /* >>> Complete rest of function >>> */
   // PERF: breaking down mask `((1 << k) - 1) << (w - k)` by parts:
-  // `(1 << k)`: shifts unique bit-1 by k-bits to the left eg. `=0000 1000`
-  // `(1 << k) - 1`: leverages addition (-1=~(1)+1) with carry out of the MSB, leaving k rightmost bits equal to ones eg. `=(+carry)0000 0111`
+  // `(1 << k)`: shifts unique bit-1 by k-bits to the left eg. `(1 << 3) = 0000 1000`
+  // `(1 << k) - 1`: leverages addition (-0b1=~(0b1)+0b1) with carry out of the MSB, leaving k rightmost bits equal to ones eg. `=(+carry)0000 0111`
   // `((1 << k) - 1) << (w - k)`: shifts all k-leftmost-ones to the left by (w-k)-bits eg. `=1110 0000`
   return xsra & ~(((1 << k) - 1) << (w - k));
   /* <<< Complete rest of function <<< */
@@ -52,11 +52,15 @@ int sra(int x, int k) {
   int xsrl = (unsigned) x >> k;
 
   /* >>> Complete rest of function >>> */
-  // grab most significant byte
-  signed char msbyte = ((unsigned char *) &x)[sizeof(int) - 1];
+  // PERF: step-by-step arithmetic fill
+  // 1. Isolate MSB from original x argument then apply double logical !! to get 0x01 if x<0 and 0x00 if x>0
+  // 2. Shift padded-sign bit with k leftmost bits to the same value of MSB (`-msb` is 0xFFFFFFFF if msb=0x01 and 0x00000000 if msb=0x00)
+  // 3. Avoid Undefined Behavior 
+  int msb = !!(x & (1 << (w - 1)));      // grab most significant bit
+  int replicate_msb = -msb; // if msb is positive then
+  int mask = ((1 << k) - 1) << (w - k);
 
-  return (int) msbyte;
-  // return xsrl & ~(((1 << k) - 1) << (w - k));
+  return xsrl | (replicate_msb & mask);
   /* <<< Complete rest of function <<< */
 }
 
