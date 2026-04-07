70ffc520bc6a020f424fde45ccf5d70a0cf5aa65
igodlab <igodlab@gmail.com>
2026-04-07 14:58:32 -0700

    Problem 2.96 init

---
+++ src/ch02/c-solutions/prob2-96.c
@@ -0,0 +1,28 @@
+/*
+2.96 ◆◆◆◆
+Following the bit-level floating-point coding rules, implement the function with 
+the following prototype:
+
+```c
+// Compute (int) f. 
+// If conversion causes overflow or f is NaN, then return 0x80000000.
+int float_f2i(float_bits f); 
+```
+
+For floating-point number f, this function computes `(int) f`. If f is NaN, your 
+function should round toward zero. If f cannot be represented as an integer (eg. it is out 
+of range, or il is NaN), then the function should return `0x80000000`. 
+Test your function by evaluating it for all 2^32 values of argument f and com­
+paring the result to what would be obtained using your machine's floating-point 
+operations.
+*/
+
+#include <stdio.h>
+
+int float_bits(float_bits f) {
+  return 0;
+}
+
+int main(void) {
+  return 0;
+}
