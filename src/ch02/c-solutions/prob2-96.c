f0e62ad78113e7bdce18b7e2166cb3a315cb52ed
igodlab <igodlab@gmail.com>
2026-04-16 15:31:35 -0700

    problem 2.96 done w/o unit tests

--- src/ch02/c-solutions/prob2-96.c
+++ src/ch02/c-solutions/prob2-96.c
@@ -26,53 +26,76 @@ operations.
 const int w = sizeof(float) << 3;
 const int exponent_bits = 8;
 const int bias = (1 << (exponent_bits - 1)) - 1;
-const int fraction_bits = 23;
+const int mantissa_bits = 23;
 
 typedef union {
   float f;
   uint32_t u;
 } float_bits;
 
-int weighted_digits(uint32_t raw_x, int E) {
-  int whole_part = 1 << E; // Account for implicit one in significand: 1.fraction
+
+// We'll express our number in the form: 
+// $$
+// b = \sum_{i=-n}^E b_i\times 2^i
+// $$
+int weighted_digits(uint32_t raw_mantissa, int E, int sign) {
+  // Account for implicit one in significand: 1.mantissa
+  int whole_part = 1 << E; // Set its value with the largest exponent ie. 1 x 2^E
+  int weight_exp = E - 1; // This is the second largest exponent base (only) ie. 2^(E-1)
   float frac_part = 0.0f;
-  int weight_exp = E - 1;
-  for (int i = fraction_bits - 1; i >= 0; i--) {
-    int positional_bit = (raw_x >> i) & 1;
-    // Check if exponent should be positive or negative
-    // printf("\ncounter = %i, E=%i", i, weight_exp);
-    if (i > fraction_bits - 1 - E) 
+
+  printf("\nimplicit digit = 1x2^%i", E);
+  for (int i = mantissa_bits - 1; i >= 0; i--) {
+    int positional_bit = (raw_mantissa >> i) & 1; // positional bit b_i
+    printf("\ncounter = %i, %01ix2^%i", i, positional_bit, weight_exp);
+
+    if (weight_exp >= 0)
     {
+      printf(", 2^i = %i", positional_bit << weight_exp);
       whole_part += positional_bit << weight_exp;
-      weight_exp--;
     } else
     {
-      frac_part += (float)positional_bit / (1 << ((int)(~weight_exp + 1)));
-      weight_exp--;
+      printf(", 2^-i = %f", (float)positional_bit / (1 << (~weight_exp + 1)));
+      frac_part += (float)positional_bit / (1 << (~weight_exp + 1));
     }
   }
 
-  printf("\nwhole_part=%i\n", whole_part);
-  printf("frac_part=%f\n", frac_part);
-  if (frac_part > 0.5) 
+  weight_exp--;
+  if (1 == sign) 
   {
-    return whole_part + 1;
+    printf("\nsign is negative");
+    printf("\nwhole_part = %i", -whole_part);
+    printf("\nfrac_part = %f\n", frac_part);
+    return -whole_part;
   } else
   {
+    printf("\nsign is positive");
+    printf("\nwhole_part = %i", whole_part);
+    printf("\nfrac_part = %f\n", frac_part);
     return whole_part;
   }
 }
 
 int float_f2i(float_bits f) {
   int S = f.u >> (w - 1);
-  unsigned int exponent_u = f.u >> (w - 1 - exponent_bits);
+  unsigned exponent_u = f.u >> (w - 1 - exponent_bits);
   int exponent = exponent_u & 0xFFu;
   int E = exponent - bias;
-  int mantissa = f.u & ((1u << fraction_bits) - 1);
+  int mantissa = f.u & ((1u << mantissa_bits) - 1);
   
   int reconstructed_bits = (S << (w - 1)) | ((E + bias) << (w - 1 - exponent_bits)) | ((unsigned)mantissa);
   // return reconstructed_bits;
 
+  // Check if exponent is negative (rounds to zero) ie. E = exponent_k - bias_k < 0
+  if (E < 0) 
+    return 0;
+
+  // Check overflow, large exponent ie. E = exponent_k - bias_k > 31
+  // why 31? Because 2^31 = 2147483648 (48 off due to error) 
+  // which overflows INT_MAX=2147483647 
+  if (E > 31)
+    return INT_MAX;
+  
   // Denormalized numbers -> all round to zero
   if (0 == exponent) 
   {
@@ -87,11 +110,10 @@ int float_f2i(float_bits f) {
       return INT_MIN;
     }
   }
-  // Exponent within range gets rounded to nearest unsigned integer
-  //
-  // 340282346638528859811704183484516925440 (FLT_MAX)
+  // Exponent within normalized range gets rounded to floor
   else {
-    return weighted_digits(f.u, E) | (S << (w - 1));
+    return weighted_digits(f.u, E, S);
+    // return cast_conversion(f.u, E);
   }
 }
 
@@ -99,7 +121,7 @@ int main(void) {
   float_bits f;
   scanf("%f", &f.f);
   print_bits(f.u);
-  print_bits(float_f2i(f));
+  // print_bits(float_f2i(f));
   printf("%i\n", float_f2i(f));
   return 0;
 }
