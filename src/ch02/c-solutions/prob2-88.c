e9f3e2c5002ddaa579431e2298a8aea894b010c4
igodlab <igodlab@gmail.com>
2026-04-20 23:01:38 -0700

    Add clean table description for problem 2.88

--- src/ch02/c-solutions/prob2-88.c
+++ src/ch02/c-solutions/prob2-88.c
@@ -15,6 +15,19 @@ IEEE floating-point format.
 In the following table, you are given some bit patterns in format A. and your task is to convert them to the closest value in format B. If rounding is necessary 
 you should round toward $+\inf$. In addition, give the values of numbers given by 
 the format A and format B bit patterns. Give these as whole numbers (e.g., 17) or as fractions (e.g., 17/64 or 17/2^6). 
+
+
+       Format A                      Format B
+-------------------------  ------------------------
+ Format A     Value A            Format B     Value B
+------------ --------------     ------------ --------------
+1 01111 001   -9/8              1 0111 0010   -9/8
+0 10110 011   ------            0 1110 0110   ------ 
+1 00111 010   ------            1 0000 0101   ------
+0 00000 111   ------            0 0000 0001   ------ 
+1 11100 000   ------            1 1111 0000   ------ 
+0 10111 100   ------            0 1111 0000   ------ 
+
 */
 
 #include <stdio.h>
@@ -44,9 +57,9 @@ void print_table(void) {
  
     ROW("1 01111 001", " -9/8",      "1 0111 0010", " -9/8");
     ROW("0 10110 011", " 176",       "0 1110 0110", " 176");
-    ROW("1 00111 010", " -5/256",    "1 0000 0101", " -5/256");
-    ROW("0 00000 111", " 7/131072",  "0 0000 0000", " 0");
-    ROW("1 11100 000", " -8192",     "1 1111 0000", " -inf");
+    ROW("1 00111 010", " -5/1024",    "1 0000 0101", " -5/1024");
+    ROW("0 00000 111", " 7/131072",  "0 0000 0001", " 1/1024");
+    ROW("1 11100 000", " -8192",     "1 1111 0000", " -248");
     ROW("0 10111 100", " 384",       "0 1111 0000", " +inf");
  
 #undef ROW
